8039336fb9800d9614ee192b9d133431fe88b2e6
Lucie Zýmová <zyminka@gmail.com>
2020-06-14 19:55:02 +0200

    body 2 a 4

---
+++ src/components/Joke.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="joke__body">
+          <img class="joke__user" src="assets/img/user01.png" />
+          <p class="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+</template>
